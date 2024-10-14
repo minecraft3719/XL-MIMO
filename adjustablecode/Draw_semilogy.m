36c9ddb9c31bd84dd5b02a284b06f969b9a85e2f
Lâm Vũ <vulam3719@gmail.com>
2024-10-14 16:18:51 +0700

    update test model

--- adjustablecode/Draw_semilogy.m
+++ adjustablecode/Draw_semilogy.m
@@ -1,7 +1,15 @@
 clc
 clear all
 close all
-drawing = table2array(readtable('nmseSummary.csv'));
+loss = table2array(readtable('D:\XL-MIMO\adjustablecode\mr_Son_training_result\loss_history.txt'));
+
+figure(1)
+plot(loss)
+legend("training loss overtime",FontSize=18)
+ylabel("nmse loss",'FontSize',16)
+xlabel("epoch cycle",'FontSize',16)
+
+
 
 % SNR = graphing(:,6);
 % estimation = graphing(:,7);
