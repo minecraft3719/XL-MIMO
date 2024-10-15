3d095fb92e8558f37929d7c9412a0f39ec1884ac
Lâm Vũ <vulam3719@gmail.com>
2024-10-15 16:24:17 +0700

    update output

--- adjustablecode/Draw_semilogy.m
+++ adjustablecode/Draw_semilogy.m
@@ -14,16 +14,16 @@ plot_file = dir([path_directory '/20241014*.csv']);
 
 figure(2)
 legend4plot = zeros(2,length(plot_file));
-Legend = cell(length(plot_file),1)
+Legend = cell(length(plot_file),1);
 for i = 1:length(plot_file)
     nmse_result = plot_file(i).name;
     nmse_result_plot = table2array(readtable(nmse_result));
     semilogy(nmse_result_plot(:,6),nmse_result_plot(:,8));
-    %legend4plot(1,i) = int2str(nmse_result_plot(1,4));
-    %legend4plot(2,i) = int2str(nmse_result_plot(1,5));
     Legend{i} = strcat("Lf = ",int2str(nmse_result_plot(1,4)),"; Ln = ",int2str(nmse_result_plot(1,5)));
     hold on
 end
+grid on
+xlabel('SNR')
+ylabel('nmse')
 legend(Legend);
-
-
+hold off
