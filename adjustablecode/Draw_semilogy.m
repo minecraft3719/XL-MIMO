c7135157c179f9ff093023e85b7025830902af26
Lâm Vũ <51980994+minecraft3719@users.noreply.github.com>
2024-10-15 23:39:30 +0700

    update requirement

--- adjustablecode/Draw_semilogy.m
+++ adjustablecode/Draw_semilogy.m
@@ -1,7 +1,7 @@
 clc
 clear all
 close all
-loss = table2array(readtable('D:\XL-MIMO\adjustablecode\mr_Son_training_result\loss_history.txt'));
+loss = table2array(readtable('mr_Son_training_result\loss_history.txt'));
 
 figure(1)
 plot(loss)
@@ -9,7 +9,7 @@ legend("training loss overtime",FontSize=18)
 ylabel("nmse loss",'FontSize',16)
 xlabel("epoch cycle",'FontSize',16)
 
-path_directory = ('D:\XL-MIMO\adjustablecode');
+path_directory = ('.');
 plot_file = dir([path_directory '/20241014*.csv']);
 
 figure(2)
